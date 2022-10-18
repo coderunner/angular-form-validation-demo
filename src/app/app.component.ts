d6d826768368bdc335ddb1db5b3afb63f2da2c40
Felix Trepanier <felixtrepanier@gmail.com>
2022-10-18 16:42:35 -0400

    version initiale

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'demo-valiation';
+  form = this.formBuilder.group({
+    name: [null, [Validators.required]],
+    age: [null, [Validators.required, Validators.min(10), Validators.max(110)]],
+  });
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  showNameRequiredError(): boolean {
+    return this.showError('name', 'required');
+  }
+
+  showAgeRequiredError(): boolean {
+    return this.showError('age', 'required');
+  }
+
+  showAgeMinError(): boolean {
+    return this.showError('age', 'min');
+  }
+
+  showAgeMaxError(): boolean {
+    return this.showError('age', 'max');
+  }
+
+  onSubmit() {
+    console.log(this.form.controls.name.errors);
+    console.log(this.form.controls.age.errors);
+    if (this.form.valid) {
+      this.form.reset();
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+
+  private showError(field: 'name' | 'age', error: string): boolean {
+    return (
+      this.form.controls[field].hasError(error) &&
+      (this.form.controls[field].dirty || this.form.controls[field].touched)
+    );
+  }
 }
