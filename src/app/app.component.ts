10f51d10bf00b1e4c0d9c0efb416003894810754
Felix Trepanier <felixtrepanier@gmail.com>
2023-10-17 11:33:54 -0400

    with Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,50 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { FormBuilder, FormGroupDirective, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  @ViewChild(FormGroupDirective)
+  formDirective: FormGroupDirective | null = null;
+
   form = this.formBuilder.group({
     name: [null, [Validators.required]],
-    age: [null, [Validators.required, Validators.min(10), Validators.max(110)]],
+    age: [
+      null,
+      [
+        Validators.required,
+        Validators.min(10),
+        Validators.max(110),
+        Validators.pattern("^[0-9]*$"),
+      ],
+    ],
   });
 
   constructor(private formBuilder: FormBuilder) {}
 
+  ngOnInit() {}
+
   showNameRequiredError(): boolean {
-    return this.showError('name', 'required');
+    return this.showError("name", "required");
   }
 
   showAgeRequiredError(): boolean {
-    return this.showError('age', 'required');
+    return this.showError("age", "required");
   }
 
   showAgeMinError(): boolean {
-    return this.showError('age', 'min');
+    return this.showError("age", "min");
   }
 
   showAgeMaxError(): boolean {
-    return this.showError('age', 'max');
+    return this.showError("age", "max");
+  }
+
+  showAgePatternError(): boolean {
+    return this.showError("age", "pattern");
   }
 
   onSubmit() {
@@ -35,12 +52,13 @@ export class AppComponent {
     console.log(this.form.controls.age.errors);
     if (this.form.valid) {
       this.form.reset();
+      this.formDirective?.resetForm();
     } else {
       this.form.markAllAsTouched();
     }
   }
 
-  private showError(field: 'name' | 'age', error: string): boolean {
+  private showError(field: "name" | "age", error: string): boolean {
     return (
       this.form.controls[field].hasError(error) &&
       (this.form.controls[field].dirty || this.form.controls[field].touched)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppComponent } from './app.component';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { AppComponent } from "./app.component";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, ReactiveFormsModule],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
